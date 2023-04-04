b96167de6d840b790bd50611e48ac7eaf937c859
MonCac <2500377984@qq.com>
2023-04-04 15:04:17 +0800

    2023.04.04 second-update

--- study_plan_algorithm_basic/No_08_001.java
+++ study_plan_algorithm_basic/No_08_001.java
@@ -2,9 +2,69 @@ package study_plan_algorithm_basic;
 
 // 1091. 二进制矩阵中的最短路径
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class No_08_001 {
-    public int shortestPathBinaryMatrix(int[][] grid) {
-        int ans = 0;
-        return ans;
+    public static void main(String[] args){
+        int[][] grid = new int[2][2];
+        grid[0] = new int[]{0,1};
+        grid[1] = new int[]{1,0};
+        shortestPathBinaryMatrix(grid);
+
+    }
+    public static int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1){
+            return -1;
+        }
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(a[i],Integer.MAX_VALUE);
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        a[0][0] = 1;
+        queue.add(new int[]{0,0});
+        while (!queue.isEmpty()){
+            int[] b = queue.poll();
+            int num = a[b[0]][b[1]];
+            int nextnum = num + 1;
+            // 进行八个方向的判断,顺序是：上、下、左、右、左上、右上、左下、右下
+            if (b[0] > 0 && grid[b[0] - 1][b[1]] == 0 && nextnum < a[b[0] - 1][b[1]]) {
+                queue.add(new int[]{b[0] - 1,b[1]});
+                a[b[0] - 1][b[1]] = nextnum;
+            }
+            if (b[0] < n - 1 && grid[b[0] + 1][b[1]] == 0 && nextnum < a[b[0] + 1][b[1]]) {
+                queue.add(new int[]{b[0] + 1,b[1]});
+                a[b[0] + 1][b[1]] = nextnum;
+            }
+            if (b[1] > 0 && grid[b[0]][b[1] - 1] == 0 && nextnum < a[b[0]][b[1] - 1]) {
+                queue.add(new int[]{b[0],b[1] - 1});
+                a[b[0]][b[1] - 1] = nextnum;
+            }
+            if (b[1] < n - 1 && grid[b[0]][b[1] + 1] == 0 && nextnum < a[b[0]][b[1] + 1]) {
+                queue.add(new int[]{b[0],b[1] + 1});
+                a[b[0]][b[1] + 1] = nextnum;
+            }
+            if (b[0] > 0 && b[1] > 0 && grid[b[0] - 1][b[1] - 1] == 0 && nextnum < a[b[0] - 1][b[1] - 1]) {
+                queue.add(new int[]{b[0] - 1,b[1] - 1});
+                a[b[0] - 1][b[1] - 1] = nextnum;
+            }
+            if (b[0] > 0 && b[1] < n - 1 && grid[b[0] - 1][b[1] + 1] == 0 && nextnum < a[b[0] - 1][b[1] + 1]) {
+                queue.add(new int[]{b[0] - 1,b[1] + 1});
+                a[b[0] - 1][b[1] + 1] = nextnum;
+            }
+            if (b[0] < n - 1 && b[1] > 0 && grid[b[0] + 1][b[1] - 1] == 0 && nextnum < a[b[0] + 1][b[1] - 1]) {
+                queue.add(new int[]{b[0] + 1,b[1] - 1});
+                a[b[0] + 1][b[1] - 1] = nextnum;
+            }
+            if (b[0] < n - 1 && b[1] < n - 1 && grid[b[0] + 1][b[1] + 1] == 0 && nextnum < a[b[0] + 1][b[1] + 1]) {
+                queue.add(new int[]{b[0] + 1,b[1] + 1});
+                a[b[0] + 1][b[1] + 1] = nextnum;
+            }
+        }
+        return a[n - 1][n - 1] == Integer.MAX_VALUE ? -1 : a[n - 1][n - 1];
     }
 }
--- study_plan_algorithm_basic/No_08_002.java
+++ study_plan_algorithm_basic/No_08_002.java
@@ -2,8 +2,63 @@ package study_plan_algorithm_basic;
 
 // 130. 被围绕的区域
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class No_08_002 {
     public void solve(char[][] board) {
-
+        int m = board.length;
+        int n = board[0].length;
+        Boolean[][] isO = new Boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            int j = 0,k = n - 1;
+            if (board[i][j] == 'O'){
+                queue.add(new int[]{i,j});
+                isO[i][j] = true;
+            }
+            if (board[i][k] == 'O' && !isO[i][k]) {
+                queue.add(new int[]{i, k});
+                isO[i][k] = true;
+            }
+        }
+        for (int i = 0; i < n; i++){
+            int j = 0, k = m - 1;
+            if (board[j][i] == 'O' && !isO[j][i]){
+                queue.add(new int[]{j,i});
+                isO[j][i] = true;
+            }
+            if (board[k][i] == 'O' && !isO[k][i]){
+                queue.add(new int[]{k,i});
+                isO[k][i] = true;
+            }
+        }
+        while (!queue.isEmpty()){
+            int[] a = queue.poll();
+            // 判断顺序为上、下、左、右
+            if (a[0] > 0 && board[a[0] - 1][a[1]] == 'O' && !isO[a[0] - 1][a[1]]){
+                queue.add(new int[]{a[0] - 1,a[1]});
+                isO[a[0] - 1][a[1]] = true;
+            }
+            if (a[0] < n - 1 && board[a[0] + 1][a[1]] == 'O' && !isO[a[0] + 1][a[1]]){
+                queue.add(new int[]{a[0] + 1,a[1]});
+                isO[a[0] + 1][a[1]] = true;
+            }
+            if (a[1] > 0 && board[a[0]][a[1] - 1] == 'O' && !isO[a[0]][a[1] - 1]){
+                queue.add(new int[]{a[0],a[1] - 1});
+                isO[a[0]][a[1] - 1] = true;
+            }
+            if (a[1] < n - 1 && board[a[0]][a[1] + 1] == 'O' && !isO[a[0]][a[1] + 1]){
+                queue.add(new int[]{a[0],a[1] + 1});
+                isO[a[0]][a[1] + 1] = true;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++){
+                if (!isO[i][j] && board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }
+            }
+        }
     }
 }
