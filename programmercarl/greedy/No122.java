dee1f0a7e82090df7967dfaa63d3008de05d3c79
MonCac <2500377984@qq.com>
2025-05-12 22:51:26 +0800

    提交两道代码随想录中有关贪心算法的题目。 1. No53: 最大子数组和 2. No122: 买卖股票的最佳时机 II

---
+++ programmercarl/greedy/No122.java
@@ -0,0 +1,19 @@
+package programmercarl.greedy;
+
+//  买卖股票的最佳时机 II
+
+public class No122 {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        if (prices.length < 2){
+            return ans;
+        }
+        for (int i = 0; i < prices.length - 1; i++){
+            int add = prices[i + 1] - prices[i];
+            if (add > 0){
+                ans += add;
+            }
+        }
+        return ans;
+    }
+}
---
+++ programmercarl/greedy/No53.java
@@ -0,0 +1,20 @@
+package programmercarl.greedy;
+
+// 最大子数组和
+
+public class No53 {
+    public int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int sum = 0;
+        for (int i : nums){
+            sum += i;
+            if (sum > ans){
+                ans = sum;
+            }
+            if (sum < 0){
+                sum = 0;
+            }
+        }
+        return ans;
+    }
+}
