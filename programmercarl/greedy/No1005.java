6139dbca2d05787c236280cd63629dc54e487763
MonCac <2500377984@qq.com>
2025-05-20 23:02:52 +0800

    提交两道代码随想录中有关贪心算法的题目。 1. No1005: K次取反后最大化的数组和（完成） 2. No134: 加油站（未完成）

--- programmercarl/greedy/No1005.java
+++ programmercarl/greedy/No1005.java
@@ -5,5 +5,43 @@ package programmercarl.greedy;
 import java.util.Arrays;
 
 public class No1005 {
-
+    public static void main(String[] args){
+        largestSumAfterKNegations(new int[]{-3,-2,-4}, 4);
+    }
+    public static int largestSumAfterKNegations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0 ){
+                if (k > 0) {
+                    ans += -nums[i];
+                    k--;
+                }else{
+                    ans += nums[i];
+                }
+            }else{
+                if (k > 0){
+                    if (k % 2 == 0){
+                        ans += nums[i];
+                    }else{
+                        if (nums[0] >= 0 || -nums[i - 1] > nums[i]) {
+                            ans += -nums[i];
+                        } else {
+                            ans += nums[i - 1] * 2;
+                            ans += nums[i];
+                        }
+                    }
+                    k = 0;
+                }else{
+                    ans += nums[i];
+                }
+            }
+        }
+        if (k > 0){
+            if (k % 2 == 1){
+                ans += nums[nums.length - 1] * 2;
+            }
+        }
+        return ans;
+    }
 }
---
+++ programmercarl/greedy/No134.java
@@ -0,0 +1,10 @@
+package programmercarl.greedy;
+
+// 134. 加油站
+
+public class No134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int ans = 0;
+        return ans;
+    }
+}
