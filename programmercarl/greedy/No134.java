d994f31cd0909d7551f29fd0555e2d8580db4fc9
MonCac <2500377984@qq.com>
2025-05-21 22:34:45 +0800

    提交两道代码随想录中有关贪心算法的题目。 1. No134: 加油站（完成） 2. No135: 分发糖果（未完成）

--- programmercarl/greedy/No134.java
+++ programmercarl/greedy/No134.java
@@ -2,9 +2,28 @@ package programmercarl.greedy;
 
 // 134. 加油站
 
+import java.util.Arrays;
+
 public class No134 {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int ans = 0;
+        int len = gas.length;
+        int[] diff = new int[len];
+        for (int i = 0; i < len; i++){
+            diff[i] = gas[i] - cost[i];
+        }
+        if (Arrays.stream(diff).sum() < 0){
+            return -1;
+        }else{
+            int sum = 0;
+            for (int i = 0; i < len; i++){
+                sum += diff[i];
+                if (sum < 0){
+                    ans = i + 1;
+                    sum = 0;
+                }
+            }
+        }
         return ans;
     }
 }
---
+++ programmercarl/greedy/No135.java
@@ -0,0 +1,10 @@
+package programmercarl.greedy;
+
+// 135. 分发糖果
+
+public class No135 {
+    public int candy(int[] ratings) {
+        int ans = 0;
+        return ans;
+    }
+}
