fc477474dfe45b90c12abda219a8d9d2f9ecc728
sulaga <2500377984@qq.com>
2025-10-24 23:53:58 +0800

    hot100 slidingwindow No438 找到字符串中所有字母异位词

--- hot100/slidingwindow/No_438.java
+++ hot100/slidingwindow/No_438.java
@@ -4,43 +4,53 @@ import java.util.*;
 
 public class No_438 {
     public static void main(String[] args) {
-        findAnagrams("abab", "ab");
+        findAnagrams("aaabaaa", "aaa");
     }
     public static List<Integer> findAnagrams(String s, String p) {
-        int lens = s.length();
-        int lenp = p.length();
-        List<Integer> ans = new ArrayList<>();
-        StringBuilder sbs = new StringBuilder();
-        StringBuilder sbp = new StringBuilder();
-        if (lens < lenp){
-            return ans;
+        int sLen = s.length(), pLen = p.length();
+
+        if (sLen < pLen) {
+            return new ArrayList<Integer>();
         }
-        for (int i = 0; i < lenp; i++){
-            sbs.append(s.charAt(i));
-            sbp.append(p.charAt(i));
+
+        List<Integer> ans = new ArrayList<Integer>();
+        int[] count = new int[26];
+        for (int i = 0; i < pLen; ++i) {
+            ++count[s.charAt(i) - 'a'];
+            --count[p.charAt(i) - 'a'];
         }
-        if (compare(sbs,sbp)){
-            ans.add(0);
-        }
-        for (int i = 1; i < lens - lenp + 1; i++){
-            sbs.deleteCharAt(0);
-            sbs.append(s.charAt(i + lenp - 1));
-            if (compare(sbs,sbp)){
-                ans.add(i);
+
+        int differ = 0;
+        for (int j = 0; j < 26; ++j) {
+            if (count[j] != 0) {
+                differ++;
             }
         }
-        return ans;
-    }
 
-    public static boolean compare(StringBuilder sb1, StringBuilder sb2) {
-        if (sb1 == null || sb2 == null) {
-            return sb1 == sb2; // 都为 null 才算相等
+        if (differ == 0) {
+            ans.add(0);
         }
-        // 顺序无关：排序后比较
-        char[] arr1 = sb1.toString().toCharArray();
-        char[] arr2 = sb2.toString().toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        return Arrays.equals(arr1, arr2);
+
+        for (int i = 0; i < sLen - pLen; ++i) {
+            if (count[s.charAt(i) - 'a'] == 1) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从不同变得相同
+                --differ;
+            } else if (count[s.charAt(i) - 'a'] == 0) {  // 窗口中字母 s[i] 的数量与字符串 p 中的数量从相同变得不同
+                ++differ;
+            }
+            --count[s.charAt(i) - 'a'];
+
+            if (count[s.charAt(i + pLen) - 'a'] == -1) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从不同变得相同
+                --differ;
+            } else if (count[s.charAt(i + pLen) - 'a'] == 0) {  // 窗口中字母 s[i+pLen] 的数量与字符串 p 中的数量从相同变得不同
+                ++differ;
+            }
+            ++count[s.charAt(i + pLen) - 'a'];
+
+            if (differ == 0) {
+                ans.add(i + 1);
+            }
+        }
+
+        return ans;
     }
 }
