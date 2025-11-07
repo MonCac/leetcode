dc0e6c738224c1f63b3280139f446769dedc88bc
sulaga <2500377984@qq.com>
2025-11-07 22:39:15 +0800

    hot100 linkedlist No23 合并K个升序链表

--- hot100/linkedlist/No_23.java
+++ hot100/linkedlist/No_23.java
@@ -16,6 +16,35 @@ public class No_23 {
         }
     }
     public ListNode mergeKLists(ListNode[] lists) {
-
+       ListNode dummy = new ListNode(0);
+       ListNode cur = dummy;
+       int sum = 0;
+       int k = lists.length;
+       for (ListNode list : lists) {
+           ListNode head = list;
+           while (head != null) {
+               sum++;
+               head = head.next;
+           }
+       }
+       while (sum > 0){
+           int min = Integer.MAX_VALUE;
+           int index = -1;
+           for (int i = 0; i < k; i++) {
+               if (lists[i] == null){
+                   continue;
+               }
+               ListNode head = lists[i];
+               if (head.val < min){
+                   min = head.val;
+                   index = i;
+               }
+           }
+           cur.next = lists[index];
+           cur = cur.next;
+           lists[index] = lists[index].next;
+           sum--;
+       }
+       return dummy.next;
     }
 }
