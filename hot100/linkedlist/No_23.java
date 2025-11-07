947424185ea5eeb8142cecde1ed112dad497c735
sulaga <2500377984@qq.com>
2025-11-07 13:20:58 +0800

    hot100 linkedlist No23 合并K个升序链表

---
+++ hot100/linkedlist/No_23.java
@@ -0,0 +1,21 @@
+package hot100.linkedlist;
+
+import java.util.List;
+
+public class No_23 {
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(){}
+        ListNode(int x){
+            val = x;
+        }
+        ListNode(int x,ListNode next){
+            this.val = x;
+            this.next = next;
+        }
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+
+    }
+}
