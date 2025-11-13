77c83e10fa11982c8aa2e28a716ad2347903cf4e
sulaga <2500377984@qq.com>
2025-11-14 00:48:22 +0800

    hot100 binary No98 验证二叉搜索树

---
+++ hot100/binarytree/No_98.java
@@ -0,0 +1,31 @@
+package hot100.binarytree;
+
+import com.sun.source.tree.Tree;
+
+public class No_98 {
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            val = x;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    public boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+}
