762a37e36fd3387b3887598612ee2b4a5a1b6ff5
sulaga <2500377984@qq.com>
2025-11-14 00:49:15 +0800

    hot100 binary No543 二叉树的直径

--- hot100/binarytree/No_543.java
+++ hot100/binarytree/No_543.java
@@ -1,7 +1,5 @@
 package hot100.binarytree;
 
-import apple.laf.JRSUIUtils;
-
 public class No_543 {
     class TreeNode{
         int val;
@@ -17,6 +15,18 @@ public class No_543 {
         }
     }
     public int diameterOfBinaryTree(TreeNode root) {
+        return messageOfBinaryTree(root)[1];
+    }
 
+    public int[] messageOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return new int[]{-1, -1};
+        }
+        int[] result = new int[2]; // 存放两个值，该节点的最长边和最大值
+        int[] ans1 = messageOfBinaryTree(root.left);
+        int[] ans2 = messageOfBinaryTree(root.right);
+        result[0] = Math.max(ans1[0], ans2[0]) + 1;
+        result[1] = Math.max(Math.max(ans1[1], ans2[1]), ans1[0] + ans2[0] + 2);
+        return result;
     }
 }
