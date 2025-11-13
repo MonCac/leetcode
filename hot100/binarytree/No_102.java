331dd3f5e88bdd4793841b882715160fd2d66d5e
sulaga <2500377984@qq.com>
2025-11-14 00:48:41 +0800

    hot100 binary No102 二叉树的层序遍历

---
+++ hot100/binarytree/No_102.java
@@ -0,0 +1,37 @@
+package hot100.binarytree;
+
+import java.util.*;
+
+public class No_102 {
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            val = x;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
