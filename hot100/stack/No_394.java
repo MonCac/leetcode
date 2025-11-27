3bebd3e54922e17783f771a871089a0df9431d9d
sulaga <2500377984@qq.com>
2025-11-28 02:35:08 +0800

    hot100 binarysearch No394 字符串解码 (未完成)

---
+++ hot100/stack/No_394.java
@@ -0,0 +1,15 @@
+package hot100.stack;
+
+import java.util.Stack;
+
+public class No_394 {
+    public String decodeString(String s) {
+        StringBuffer ans = new StringBuffer();
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        int sum = 0;
+        int flag = 0;
+        StringBuffer sb1 = new StringBuffer();
+        return ans.toString();
+    }
+}
