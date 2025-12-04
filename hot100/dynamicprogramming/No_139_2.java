55ad83b97c2526924e3cc874f02f59f556b649fb
sulaga <2500377984@qq.com>
2025-12-05 02:45:31 +0800

    hot100 greed No139 单词拆分(动态规划)

---
+++ hot100/dynamicprogramming/No_139_2.java
@@ -0,0 +1,25 @@
+package hot100.dynamicprogramming;
+
+import java.util.*;
+
+public class No_139_2 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < wordDict.size(); i++){
+            set.add(wordDict.get(i));
+        }
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+        for (int i = 1; i < f.length; i++){
+            for (String str : set){
+                if (i >= str.length() && f[i - str.length()]){
+                    if (s.substring(i-str.length(), i).equals(str)){
+                        f[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return f[s.length()];
+    }
+}
