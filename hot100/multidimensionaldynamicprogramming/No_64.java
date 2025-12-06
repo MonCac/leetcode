22c488669052e1a344ce3f258e572c6358edba98
sulaga <2500377984@qq.com>
2025-12-07 02:48:16 +0800

    hot100 multidimensionaldynamicprogramming No64 最小路径和

---
+++ hot100/multidimensionaldynamicprogramming/No_64.java
@@ -0,0 +1,25 @@
+package hot100.multidimensionaldynamicprogramming;
+
+public class No_64 {
+    public int minPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
+                if (i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                if (i == 0){
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                    continue;
+                }
+                if (j == 0){
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                    continue;
+                }
+                dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+            }
+        }
+        return dp[grid.length-1][grid[0].length-1];
+    }
+}
