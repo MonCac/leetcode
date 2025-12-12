75141b8d82012b34abed0b93563a2c06c9340b52
sulaga <2500377984@qq.com>
2025-12-12 23:57:01 +0800

    hot100 trick No136 只出现一次的数字

---
+++ hot100/trick/No_136.java
@@ -0,0 +1,11 @@
+package hot100.trick;
+
+public class No_136 {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums){
+            ans ^= num;
+        }
+        return ans;
+    }
+}
